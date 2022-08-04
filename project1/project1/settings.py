96efa2ef4a6770cd4409be0da8892cbc18737458
unknown <devilsaumil11@gmail.com>
2022-08-04 15:08:03 +0530

    first release

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -124,7 +124,7 @@ VENV_PATH=os.path.dirname(BASE_DIR)
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(VENV_PATH,'static_root')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATICFILES_STORAGE="whitenoise.storage.CompressedMainfestStaticFileStorage"
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFileStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
